c5c1f6f0db7c8fae0b2e3c1d43d049500771a9e6
Haik D <haikadivanyan@gmail.com>
2020-10-22 16:19:08 -0700

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+let haik = 'haik'
 
 const PORT = 3000;
 const URI = "mongodb://127.0.0.1:27017/names";
